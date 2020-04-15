224165117546a71910c293730253449a7e8cc1f1
Latosha Tims <ltims080913@hotmail.com>
2020-04-15 15:26:39 -0700

    fixed issue with compiling

--- app/src/components/DumpQuote.js
+++ app/src/components/DumpQuote.js
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import Loader from 'react-loader-spinner';
-import {fetchingQuote} from '../store/actions/quoteActions';
+import {fetchQuote} from '../store/actions/quoteAction';
 
 const DumpQuote = props => {
     useEffect(() => {
         props.fetchQuote();
+        
     }, []);
 
     return (
@@ -16,14 +17,14 @@ const DumpQuote = props => {
             )}
             {props.quote && <h3>"{props.quote}"</h3>}
             {props.error && <p className="error">{props.error}</p>}
-            <button onClick={props.fetchQuote}>Fetch a new quote</button>
+            <button onClick={props.fetchQuote}>What else does Orangie say?</button>
         </div>
     )
 }
 const mapStateToProps = state => {
     console.log(state);
     return {
-      quote: state.quotes.quote,
+      value: state.quotes.value,
       isFetching: state.quotes.isFetching,
       error: state.quotes.error
     };
--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from '.store/reducers';
+import rootReducer from './store/reducers';
 
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
